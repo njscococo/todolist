e5cb086a45d8e6d966fa61143f3c04c2c253c636
voyagerlin <voyagerlin@gmail.com>
2019-04-04 17:36:20 +0800

    adding project is done

--- src/Main.js
+++ src/Main.js
@@ -13,8 +13,10 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import ExpandLess from '@material-ui/icons/ExpandLess';
 import ExpandMore from '@material-ui/icons/ExpandMore';
-import MailIcon from '@material-ui/icons/Mail';
+import AssignmentIcon from '@material-ui/icons/Assignment';
+import PersonIcon from '@material-ui/icons/Person';
 import MenuIcon from '@material-ui/icons/Menu';
+import AddIcon from '@material-ui/icons/Add';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
@@ -22,6 +24,8 @@ import { withStyles } from '@material-ui/core/styles';
 //var data = require('./data')
 import defaultData from './data';
 import { Collapse } from '@material-ui/core';
+
+import CandidateItem from './uicomponent/CandidateItem'
 
 const drawerWidth = 240;
 
@@ -64,7 +68,12 @@ class ResponsiveDrawer extends React.Component {
   state = {
     mobileOpen: false,
     projectList: defaultData.data[0].projects,
-    showProject: false
+    showProject: false,
+    isOpenAdd: false,
+    candidateProject: {
+      projectName: 'personal',
+      todoItems: [{ label: '', detail: '' }]
+    }
   };
 
   handleDrawerToggle = () => {
@@ -77,41 +86,48 @@ class ResponsiveDrawer extends React.Component {
     })
   }
 
+  addProject = (item) => {
+    this.setState((state) => {
+      return { projectList: [...state.projectList,item ]}
+    })
+  }
+
+  cancelNewProject = () => this.setState((state) => {
+    return { isOpenAdd: false}
+  })
+
   render() {
     const { classes, theme } = this.props;
-    console.log(classes);
-    console.log(theme);
+    //console.log(classes);
+    //console.log(theme);
 
     const drawer = (
       <div>
         <div className={classes.toolbar} ></div>
         <Divider />
         <List>
-          <ListItem button onClick={this.handleShowProject}>
+          <ListItem button >
             <ListItemIcon><InboxIcon /></ListItemIcon>
             <ListItemText primary='Projects' />
-            {this.state.showProject ? <ExpandLess /> : <ExpandMore />}
+            <AddIcon onClick={() => this.setState((state) => {
+              return { isOpenAdd: true}
+            })}/>
+            {this.state.showProject ? <ExpandLess onClick={this.handleShowProject} /> : <ExpandMore onClick={this.handleShowProject} />}
           </ListItem>
           <Collapse in={this.state.showProject} timeout="auto" unmountOnExit>
             <List component='div' disablePadding>
+              {this.state.isOpenAdd ? (<CandidateItem items={this.state.projectList} 
+              isOpen={this.cancelNewProject} addProject={this.addProject}/>) : null }
+              
               {this.state.projectList.map((project, index) => (
                 <ListItem button key={project.projectName} className={classes.nested}>
-                  <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                  <ListItemIcon>{index % 2 === 0 ? <PersonIcon /> : <AssignmentIcon />}</ListItemIcon>
                   <ListItemText primary={project.projectName} />
                 </ListItem>
               ))}
             </List>
           </Collapse>
-        </List>
-        <Divider />
-        <List>
-          {['All mail', 'Trash', 'Spam'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
+        </List>        
       </div>
     );
 
@@ -135,6 +151,7 @@ class ResponsiveDrawer extends React.Component {
         </AppBar>
         <nav className={classes.drawer}>
           {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+
           <Hidden smUp implementation="css">
             <Drawer
               container={this.props.container}
@@ -164,27 +181,11 @@ class ResponsiveDrawer extends React.Component {
         <main className={classes.content}>
           <div className={classes.toolbar} />
           <Typography paragraph>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent
-            elementum facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in
-            hendrerit gravida rutrum quisque non tellus. Convallis convallis tellus id interdum
-            velit laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing.
-            Amet nisl suscipit adipiscing bibendum est ultricies integer quis. Cursus euismod quis
-            viverra nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum leo.
-            Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus
-            at augue. At augue eget arcu dictum varius duis at consectetur lorem. Velit sed
+         gue eget arcu dictum varius duis at consectetur lorem. Velit sed
             ullamcorper morbi tincidunt. Lorem donec massa sapien faucibus et molestie ac.
           </Typography>
           <Typography paragraph>
-            Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
-            facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
-            tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat
-            consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus
-            sed vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in.
-            In hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
-            et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique
-            sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo
-            viverra maecenas accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam
+            maecenas accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam
             ultrices sagittis orci a.
           </Typography>
         </main>
---
+++ src/uicomponent/CandidateItem.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button'
+import AddCircleIcon from '@material-ui/icons/AddCircle';
+import DeleteIcon from '@material-ui/icons/Delete';
+
+const styles = {
+    button: {
+        margin: '2px',
+    },
+    input: {
+        display: 'none',
+    },
+};
+function CandidateItem(props) {
+    const [itemtName, setItemName] = useState('')
+
+    return (
+        <div>
+            <TextField
+                id="candidateItem"
+                label="Project Name"
+                //className={styles.button}
+                value={itemtName}
+                onChange={(evt) => {
+                    //console.log(itemtName)
+                    setItemName(evt.target.value);
+                }}
+                margin="normal"
+                variant="outlined"
+            />
+            <Button variant="contained" color="primary" style={styles.button}
+                onClick={() => {
+                    props.addProject({
+                        projectName: itemtName,
+                        todoItems: [{ label: '', detail: '' }]
+                    });
+                    setItemName('');
+                }}
+            >
+                <AddCircleIcon style={{ marginRight: 5 }} />
+                Add
+            </Button>
+            <Button variant="contained" color="secondary" style={styles.button}
+                onClick={() => props.isOpen()}
+            >
+                <DeleteIcon style={{ marginRight: 5 }} />
+                Cancel
+            </Button>
+        </div>
+    )
+}
+
+CandidateItem.propTypes = {
+    items: PropTypes.array,
+    isOpen: PropTypes.func.isRequired
+
+}
+
+export default CandidateItem;
