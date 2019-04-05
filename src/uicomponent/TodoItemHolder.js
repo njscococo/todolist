e436b80b544dd119883de7471c0a7569fab399f9
voyagerlin <voyagerlin@gmail.com>
2019-04-05 15:58:23 +0800

    adding TodoItem

--- src/uicomponent/TodoItemHolder.js
+++ src/uicomponent/TodoItemHolder.js
@@ -4,18 +4,25 @@ import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 // import SearchBar from './SearchBar'
 import Button from '@material-ui/core/Button';
+import TodoItem from './TodoItem';
+
+
 
 function TodoItemHolder(props) {
-    const { projectName } = props.project
+    const { project, addItem } = props
+    const projectName = project.projectName
+
+
     return (
         <Paper >
-            <Typography variant="h5" component="h3" align="left" color='textPrimary'>
+            <Typography variant="h5" component="h3" align="left" color='textPrimary' style={{ marginLeft: 10 }}>
                 {projectName}
-                {projectName?<Button color="primary" >
-                    Add Task
-                </Button>: null}
-                
+            </Typography >
+
+            <Typography variant="body1" component="div" align="left" style={{ marginLeft: 10 }}>
+                {projectName ? <TodoItem items={project} addItem={addItem}></TodoItem> : null}
             </Typography>
+
 
         </Paper>
     )
